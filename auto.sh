2c6f762bbec321ca5e97767cc390997ee4d0f7f1
mjgalindo <mjgalindoramos@gmail.com>
2018-03-01 19:17:27 +0100

    First attempts

---
+++ auto.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./hcode.py a_example.in > a_example.out
+./hcode.py b_should_be_easy.in > b_should_be_easy.out
+./hcode.py c_no_hurry.in > c_no_hurry.out
+./hcode.py d_metropolis.in > d_metropolis.out
+./hcode.py e_high_bonus.in > e_high_bonus.out
