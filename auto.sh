482ff9fd02aa2268d4224ea8e66b7b4582a05775
mjgalindo <mjgalindoramos@gmail.com>
2018-03-01 20:32:43 +0100

    Better approx 20mill

--- auto.sh
+++ auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./hcode.py a_example.in > a_example.out
-./hcode.py b_should_be_easy.in > b_should_be_easy.out
-./hcode.py c_no_hurry.in > c_no_hurry.out
-./hcode.py d_metropolis.in > d_metropolis.out
-./hcode.py e_high_bonus.in > e_high_bonus.out
+./hcode.py a_example.in > a_example.out && echo "a done" 
+./hcode.py b_should_be_easy.in > b_should_be_easy.out  && echo "b done" 
+./hcode.py c_no_hurry.in > c_no_hurry.out  && echo "c done" 
+./hcode.py d_metropolis.in > d_metropolis.out  && echo "d done" 
+./hcode.py e_high_bonus.in > e_high_bonus.out  && echo "e done" 
--- hcode.py
+++ hcode.py
@@ -2,29 +2,56 @@
 import sys
 import math
 
-if len(sys.argv) < 2:
+
+def fdistance(l1, l2):
+    return abs(l1[0] - l2[0]) + abs(l1[1] - l2[1])
+
+"""if len(sys.argv) < 3:
     print("Not enough params")
+    exit(1)"""
 
-data = []
+fname = "a_example.in"
+if len(sys.argv) > 1:
+    fname = sys.argv[1]
 
-with open(sys.argv[1], 'r') as f:
+rideData = {}
+
+with open(fname, 'r') as f:
     # 3 rows, 4 columns, 2 vehicles, 3 rides, 2 bonus and 10 steps
-    rows, columns, vehicles, rides, bonus, steps = [int(x) for x in f.readline()[0:-1].split(" ")]
+    rows, columns, nvehicles, rides, bonus, steps = [int(x) for x in f.readline()[0:-1].split(" ")]
+    vehicles = []
+    for i in range(nvehicles):
+        vehicles.append([[0,0], 0, []])
     lines = f.readlines()
-    for line in lines:
+    for index, line in enumerate(lines):
         vals = [int(x) for x in line[0:-1].split(" ")]
         start = vals[0:2]
         end = vals[2:4]
         earlyStart = int(vals[4])
         earlyEnd = int(vals[5])
-        data.append([start, end, earlyStart, earlyEnd])
+        distance = fdistance(start, end)
+        rideData[index] = [index, start, end, earlyStart, earlyEnd, distance]
 
-rideVehi = int(math.ceil(float(rides) / float(vehicles)))
+for step in range(steps):
+    if len(rideData) == 0:
+        break
+    for v in vehicles:
+        smallestDist = 999999999
+        nearestI = -1
+        for k, ride in rideData.items():
+            distance = fdistance(v[0], ride[1])
+            if distance < smallestDist:
+                smallestDist = distance
+                nearestI = ride[0]
+        if nearestI != -1:
+            a = rideData[nearestI]
+            v[1] = a[5]
+            v[2].append(a[0])
+            v[0] = a[2]
+            del rideData[nearestI]
 
-for i in range(vehicles):
-    tmp = range(i,rides,vehicles)
-    sys.stdout.write(str(len(tmp))+ " " )
-    for i in tmp:
-        sys.stdout.write(str(i) + " ")
-    sys.stdout.write("\n")
-    
+for v in vehicles:
+    sys.stdout.write(str(len(v[2])) + " ")
+    for e in v[2]:
+        sys.stdout.write(str(e) + " ")
+    sys.stdout.write('\n')
